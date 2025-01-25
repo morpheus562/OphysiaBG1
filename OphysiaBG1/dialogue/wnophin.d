d19fcb34db013b7b289045f7836e37999f242db2
Llewren <llewrensmb@gmail.com>
2025-01-26 01:32:45 +1100

    Dialogue tweaks

--- OphysiaBG1/dialogue/wnophin.d
+++ OphysiaBG1/dialogue/wnophin.d
@@ -4,6 +4,11 @@ BEGIN WNOPHIN
 	// Ophysia's BG:EE Interjections //
 	///////////////////////////////////
 
+// Neera's joining sequence
+
+I_C_T ~NEERA~ 4 wn_ophysia_neera_join
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Or, failing that, a blade.~
+END
 
 // Dorn's joining sequence
 
@@ -28,6 +33,14 @@ I_C_T ~NEERAJ~ 170 wn_ophysia_neera_bae_2
 == WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~With a 'd', unfortunately.~
 END
 
+I_C_T ~NEERAJ~ 162 wn_ophysia_neera_bae_2
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~I've seen better wordsmithing in alleyways in Waterdeep.~
+END
+
+I_C_T ~NEERAJ~ 169 wn_ophysia_neera_bae_2
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~I've seen better wordsmithing in alleyways in Waterdeep.~
+END
+
 // Xan's joining sequence
 
 I_C_T ~XAN~ 2 wn_ophysia_xan_join
@@ -35,10 +48,85 @@ I_C_T ~XAN~ 2 wn_ophysia_xan_join
 == XAN ~Even in the depths of this place I cannot escape half-elven wit.~
 END
 
+// Gurke
+
+I_C_T ~GURKE~ 1 wn_ophysia_gurke_1
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Cloakwood’s a TERRIBLE place to lose a cloak. How would you even narrow it down?~
+== GURKE ~Oh, yer a riot, yer a riot. Move!~
+END
+
+I_C_T2 ~GURKE~ 4 wn_ophysia_gurke_2
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~I think you're vastly overestimating how stable an income 'telling people you lost a cloak in Cloakwood' is.~
+== CORANJ IF ~InParty("CORAN") InMyArea("CORAN") !StateCheck("CORAN",CD_STATE_NOTVALID) THEN~ ~Says the bard whose stories amount to hitting people in the face...~
+END
+
 // Hobgoblin took 25 gp
 
 I_C_T2 ~HOBGO5~ 1 wn_ophysia_hobgob
 == WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Way to improve group morale, <CHARNAME>.~
+END
+
+// Bassilus
+
+I_C_T2 ~BASSIL~ 10 wn_ophysia_bassilus_1
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~You'll not be taking anyone else, madman. Prepare to meet your god!~
+END
+
+I_C_T2 ~BASSIL~ 11 wn_ophysia_bassilus_2
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~You'll not be taking anyone else, madman. Prepare to meet your god!~
+END
+
+I_C_T2 ~BASSIL~ 12 wn_ophysia_bassilus_3
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~You'll not be taking anyone else, madman. Prepare to meet your god!~
+END
+
+I_C_T2 ~BASSIL~ 13 wn_ophysia_bassilus_4
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~You'll not be taking anyone else, madman. Prepare to meet your god!~
+END
+
+I_C_T2 ~BASSIL~ 14 wn_ophysia_bassilus_5
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~You'll not be taking anyone else, madman. Prepare to meet your god!~
+END
+
+// Drizzt
+
+I_C_T2 ~DRIZZT~ 10 wn_ophysia_drizzt
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Fighting alongside Drizzt Do'Urden... I'll be telling this one for a while.~
+END
+
+// Nashkel Mines Guard
+
+I_C_T2 ~AMNIS3~ 0 wn_ophysia_amnis
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Thanks for the vote of confidence.~
+END
+
+// Mulahey
+I_C_T2 ~MULAHE~ 2 wn_ophysia_mulahey
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Say your prayers while you can. You're done here!~
+END
+
+// Taugosz
+I_C_T ~TAUGOS~ 2 wn_ophysia_taugosz
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Sound advice. Chill aren't worth our time, anyway.~
+END
+
+// Bentha
+I_C_T2 ~BENTHA~ 1 wn_ophysia_bentha_1
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Thank you, m'lady. I'm glad that we were able to step in when we did.~
+END
+
+I_C_T2 ~BENTHA~ 2 wn_ophysia_bentha_2
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Thank you, m'lady. I'm glad that we were able to step in when we did.~
+END
+
+I_C_T2 ~BENTHA~ 3 wn_ophysia_bentha_3
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Thank you, m'lady. I'm glad that we were able to step in when we did.~
+END
+
+// Great Gazib
+
+I_C_T2 ~GAZIB~ 1 wn_ophysia_gazib
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Do we have to? I think I got ogre bits on me last time.~
 END
 
 // Drienn's cat
@@ -53,10 +141,28 @@ I_C_T ~SENIYA~ 0 wn_ophysia_seniyad
 == WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~As much as I'd like to see Mr. Sashenstar here have his teeth kicked in, allowing the druids to kill him may make things worse. The noble families of Baldur's Gate like to hold grudges.~
 END
 
+I_C_T2 ~ALDETH~ 6 wn_ophysia_seniyad
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Oh, hells. I guess we're doing this.~
+END
+
+// Drasus
+
+I_C_T2 ~DRASUS~ 2 wn_ophysia_drasus
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Charming. Just charming. Let's skip to the part where you're face down in the mud.~
+== XANJ IF ~InParty("XAN") InMyArea("XAN") !StateCheck("XAN",CD_STATE_NOTVALID)~ THEN ~Do try not to aggrevate them further, won't you?~
+END
+
 // Farmer Brunn
 
 I_C_T2 ~FARMBR~ 6 wn_ophysia_brunn
 == WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~I'm worried that we won't be able to deliver good news, but... this is good of you, <CHARNAME>.~
+END
+
+// Fishermen
+
+I_C_T ~JEBADO~ 6 wn_ophysia_jebado
+== WNOPHYJ IF ~InParty("WNOPHY") InMyArea("WNOPHY") !StateCheck("WNOPHY",CD_STATE_NOTVALID)~ THEN ~Hah! As if crawling to a priest of Talos to solve your money troubles was ever going to do anything? And now you're wasting MORE money to try and have a child murdered.~
+== JEBADO ~You... you—~
 END
 
 // Scar
--- OphysiaBG1/dialogue/wnophy.d
+++ OphysiaBG1/dialogue/wnophy.d
@@ -14,8 +14,8 @@ CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) GlobalLT("Chapter","GLOBAL",3) NumTime
 CHAIN WNOPHY Join02
 ~Very well. Where was I?~
 == MTOWNA ~Yer sword broke.~
-== WNOPHY ~Right! With one half of my brand new sword in my hand, and the other—the more deadly half—embedded in my attacker's torso, I was rushed from behind by his partner.~
-== WNOPHY ~I turned at just the right moment, though still stunned by what had just happened, and swung my trusty buckler directly into my attacker's jaw, knocking him clean out!~
+== WNOPHY ~Right! With one half of my brand new sword in my hand, and the other, more deadly half embedded in my attacker's torso, I was rushed from behind by his partner.~
+== WNOPHY ~I turned at last moment, still stunned by what had just happened, and swung my buckler directly into the man's jaw, knocking him clean out!~
 == MTOWNA ~Hear hear!~
 == BART2 ~Sounds a bit far fetched, if you ask me.~
 == VOLO ~A fine story, my dear.~
@@ -26,9 +26,10 @@ CHAIN WNOPHY Join02
 		IF~~THEN REPLY ~I just wanted to listen in. I'll be going now.~ EXTERN WNOPHY Join03
 
 CHAIN WNOPHY Join04
-~The name's Ophysia Lorines, a warrior-storyteller and recently of Daggerford. I've come south to investigate the iron crisis that seems to be causing the increase in bandit activity. So far I've had little luck, but I've managed to hold my own against them—even if it's cost me a sword or two. Now, who might you be?~
+~The name's Ophysia Lorines, a warrior-storyteller and recently of Daggerford. I've come south to put a blade in the bandit activity across the Coast Way. So far I've had little luck, outside of the occasional scuffle. What of you?~
 	END
 		IF~~THEN REPLY ~I'm <CHARNAME>, of Candlekeep. I'm here to look into the mines I've heard about.~ EXTERN WNOPHY Join06
+		IF~~THEN REPLY ~I'm just a traveler, come to Nashkel to investigate the mines south of here.~ EXTERN WNOPHY Join06
 		IF~~THEN REPLY ~Someone with somewhere else to be. Good day.~ EXTERN WNOPHY Join03
 
 CHAIN WNOPHY Join05 
@@ -38,7 +39,7 @@ CHAIN WNOPHY Join05
 		IF~~THEN REPLY ~Right. Well, I think I should be going.~ EXTERN WNOPHY Join03
 
 CHAIN WNOPHY Join06
-~Is that right? It seems that our interests align, then. Perhaps you'd be interested in having me tag along for your little escapade? Our chances at tackling the mines together seem vastly more promising than going it alone.~
+~Is that right? It seems that our interests align, then. With the increase in bandit operations, so too does the lack of quality iron. It makes for an intriguing mystery—one I'd most like to get to the bottom of. Perhaps you'd be interested in having me tag along for your little escapade? Our chances at tackling the mines together seem a mite more promising than going it alone.~
 	END
 		IF~~THEN REPLY ~I'll need all the help that I can get, so you're welcome to join me.~ EXTERN WNOPHY Join07
 		IF~~THEN REPLY ~I don't think that would be a good idea.~ EXTERN WNOPHY Join03
@@ -48,7 +49,7 @@ CHAIN WNOPHY Join07
 DO ~AddJournalEntry(@1, INFO) SetGlobal("WNOPHYJoin","GLOBAL",1) JoinParty()~ EXIT
 
 CHAIN WNOPHY Join03
-~Whatever you say! I'll be here a for while.~
+~Sure, fine. Whatever you say. I'll likely be here for a time longer. The drink is good and the patronage is appreciative—though I do have some staunch competition. I'll see you another time, perhaps.~
 EXIT
 
 // Nashkel Mines has been cleared
@@ -65,20 +66,20 @@ CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) GlobalGT("Chapter","GLOBAL",2) NumTime
 CHAIN WNOPHY JoinLate01
 ~Very well. Where was I?~
 == MTOWNA ~Yer sword broke.~
-== WNOPHY ~Right! With one half of my brand new sword in my hand, and the other—the more deadly half—embedded in my attacker's torso, I was rushed from behind by his partner.~
-== WNOPHY ~I turned at just the right moment, though still stunned by what had just happened, and swung my trusty buckler directly into the second bandit's jaw, knocking him clean out!~
+== WNOPHY ~Right! With one half of my brand new sword in my hand, and the other, more deadly half embedded in my attacker's torso, I was rushed from behind by his partner.~
+== WNOPHY ~I turned at last moment, still stunned by what had just happened, and swung my buckler directly into the man's jaw, knocking him clean out!~
 == MTOWNA ~Hear hear!~
 == BART2 ~Sounds a bit far fetched, if you ask me.~
 == VOLO ~A fine story, my dear.~
 == WNOPHY ~Thank you, everyone! Please, enjoy your drinks. Now, what can I—~
-== WNOPHY ~Wait, aren't you the mercenaries that the folks around here have been talking about? Word is that someone solved a spot of trouble down at the mines, with a description that matches yours.~
+== WNOPHY ~Wait, aren't you the one that the folks around here have been talking about? Word is that a gang of mercenaries solved a spot of trouble down at the mines, with a description that matches yours.~
 	END
 		IF~~THEN REPLY ~Indeed, the mines were our doing. What's it to you?~ EXTERN WNOPHY JoinLate02
 		IF~~THEN REPLY ~I'm not entirely comfortable with being called a mercenary, but yes, that was us.~ EXTERN WNOPHY JoinLate02
 		IF~~THEN REPLY ~Nope, that was someone else. Good day.~ EXTERN WNOPHY Join03
 
 CHAIN WNOPHY JoinLate02
-~Let me introduce myself—my name is Ophysia Lorines, a warrior-storyteller, recently of Daggerford. I'm in this region to investigate the iron crisis, and it seems you are too, whether intentional or not. Perhaps it would be in our best interests to join forces?~
+~Let me introduce myself—my name is Ophysia Lorines, a warrior-storyteller, recently of Daggerford. I've come south to put a blade in the bandit activity across the Coast Way, and it seems you are too, whether intentionally or not. Perhaps it would be in our best interests to join forces?~
 	END
 		IF~~THEN REPLY ~I need all the help I can get. You're welcome to join me.~ EXTERN WNOPHY JoinLate03
 		IF~~THEN REPLY ~Perhaps another time. I can't spare the room at the moment.~ EXTERN WNOPHY JoinLate04
@@ -97,12 +98,12 @@ CHAIN WNOPHY JoinLate05
 DO ~EscapeArea()~ EXIT
 
 CHAIN WNOPHY JoinLateNo
-~Whatever you say! I'll be here a for while.~
+~Whatever you say! I'll likely be here for a time longer. The drink is good and the patronage is appreciative—though I do have some staunch competition. I'll see you another time, perhaps.~
 EXIT
 
 // Return
 
-CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) NumTimesTalkedToGT(0) ReputationGT(player1, 8)~ THEN WNOPHY Return00
+CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) NumTimesTalkedToGT(0) ReputationGT(player1, 7)~ THEN WNOPHY Return00
 ~Why, hello there. Looking for another sword arm to fight by your side? I've heard good things about your company, and I'd appreciate the chance to see you in action up close.~
 	END
 		IF~~THEN REPLY ~I need all the help I can get. You're welcome to join me.~ EXTERN WNOPHY Return01
@@ -121,6 +122,6 @@ CHAIN WNOPHY Return03
 ~Ah, a charmer. Well, I’ll leave you to dig yourself out of your messes then. Good luck with that, truly.~
 DO ~EscapeArea()~ EXIT
 
-CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) NumTimesTalkedToGT(0) ReputationLT(player1, 9)~ THEN WNOPHY ReturnBad01
+CHAIN IF ~Global("WNOPHYJoin","GLOBAL",0) NumTimesTalkedToGT(0) ReputationLT(player1, 8)~ THEN WNOPHY ReturnBad01
 ~Oh, I've heard of you. Not good things, either. Perhaps it'd be better if you moved on, no?~
 EXIT
